30a7583d8c8005bbf81037b065657ac02e27a928
Lorenzo Gaviglio <lorenzotgaviglio97@gmail.com>
2024-12-01 23:10:12 +0100

    Agrego dependencias (faltan dependencias para hacer test)

---
+++ zara-product-price-management/src/main/java/dev/price_management_api/zara_product_price_management/ZaraProductPriceManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.price_management_api.zara_product_price_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraProductPriceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaraProductPriceManagementApplication.class, args);
+	}
+
+}
---
+++ zara-product-price-management/src/test/java/dev/price_management_api/zara_product_price_management/ZaraProductPriceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.price_management_api.zara_product_price_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaraProductPriceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
